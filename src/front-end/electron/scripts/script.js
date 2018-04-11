dca40a348794ad37735382cea2ecd925b6f7669b
bosear <boldyref5@mail.ru>
2018-04-12 03:46:04 +0400

    improvements

--- src/front-end/electron/scripts/script.js
+++ src/front-end/electron/scripts/script.js
@@ -5,3 +5,7 @@ ipcRenderer.on('ready', (event) => {
     var loader = document.getElementById('loader');
     loader.remove();
 });
+
+var el = document.querySelector('.tabs');
+var instance = M.Tabs.init(el);
+console.log(instance);
